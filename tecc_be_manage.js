9a2d33cf6aa99ecce6f0d6d981366130fb40b637
S. Massy <smassy@gmail.com>
2018-04-27 15:11:55 -0400

    Initial commit

---
+++ tecc_be_manage.js
@@ -0,0 +1,13 @@
+/**
+ * Internet 2 Final Project
+ * The Electric Car Company
+ * Authors: Carvalho-Dagenais, Matthew; Massy, sebastien; Patel, Parimal
+ * Description: This scripts manages the functionalities associated with the
+ * HTML file of the same name; primarily adding/removing/reordering items and
+ * uddating their information.
+ */
+
+'use strict';
+
+function addItem() {
+
