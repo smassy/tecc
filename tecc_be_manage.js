2da48a37895fa6b3e3e873221d26d5c34b99973e
S. Massy <smassy@gmail.com>
2018-04-28 17:24:23 -0400

    - Implements generation of inventory tables.

--- tecc_be_manage.js
+++ tecc_be_manage.js
@@ -89,12 +89,116 @@ function addItem() {
 }
 
 function setup() {
+	populateInventory();
 	var submitBtn = document.getElementById("isubmit");
 	if (submitBtn.addEventListener) {
 		submitBtn.addEventListener("click", addItem, false);
 	}
 }
 
+/**
+ * Generates a table row from an inventory item. Requires and item and its index in its type category as argument.
+*/
+function getRowFromItem(item, nthOfType) {
+	var row = document.createElement("tr");
+	row.id = item.type + "-" + item.name.replace(/ /g, "_");
+	var td;
+	td = document.createElement("td");
+	// Action buttons
+	row.appendChild(td);
+	td = document.createElement("td");
+	td.innerHTML = item.name;
+	row.appendChild(td);
+	td = document.createElement("td");
+	td.innerHTML = item.type;
+	row.appendChild(td);
+	td = document.createElement("td");
+	td.innerHTML = "$" + item.price.toLocaleString();
+	row.appendChild(td);
+	td = document.createElement("td");
+	td.innerHTML = item.turnaround;
+	row.appendChild(td);
+	td = document.createElement("td");
+	td.innerHTML = item.reorder;
+	row.appendChild(td);
+	td = document.createElement("td");
+	td.innerHTML = item.stock;
+	row.appendChild(td);
+	td = document.createElement("td");
+	// action button
+	row.appendChild(td);
+	return row;
+}
+
+/**
+ * Generates an inventory table from an itemGroup object. Requires said group as parameter.
+*/
+function getTableFromGroup(group) {
+	var table = document.createElement("table");
+	table.appendChild(document.createElement("caption"));
+	var thead = document.createElement("thead");
+	var th = document.createElement("th");
+	thead.appendChild(th);
+	th = document.createElement("th");
+	th.innerHTML = "Name";
+	thead.appendChild(th);
+	th = document.createElement("th");
+	th.innerHTML = "Type";
+	thead.appendChild(th);
+	th = document.createElement("th");
+	th.innerHTML = "Price";
+	thead.appendChild(th);
+	th = document.createElement("th");
+	th.innerHTML = "Turnaround time<br />in days";
+	thead.appendChild(th);
+	th = document.createElement("th");
+	th.innerHTML = "Reorder time<br />in days";
+	thead.appendChild(th);
+	th = document.createElement("th");
+	th.innerHTML = "Stock";
+	thead.appendChild(th);
+	th = document.createElement("th");
+	thead.appendChild(th);
+	table.appendChild(thead);
+	var tbody = document.createElement("tbody");
+	for (var i = 0; i < group.count; i++) {
+		var typeArray = group[group.types[i]];
+		for (var j = 0; j < typeArray.length; j++) {
+			var row = getRowFromItem(typeArray[j], j);
+			tbody.appendChild(row);
+		}
+	}
+	table.appendChild(tbody);
+	return table;
+}
+
+function populateInventory() {
+	// Remove tables if already present
+	var tables = document.getElementsByTagName("table");
+	for (var i = 0; i < tables.length; i++) {
+		tables[i].parentNode.removeChild(tables[i]);
+	}
+	var baseDiv = document.getElementById("baseItems");
+	var p = document.createElement("p");
+	p.innerHTML = "There are currently no  items defined; please add some if you wish your customers to be able to place an order. here are a few things to remember:<ul><li>Each combination of name and type must be unique.</li><li>types may not contain blank spaces.</li><li>Types are used to group items together so that the customer may choose one of them, which means:</li><ul><li>Types holding multiple items will be presented to the customer as an option group.</li><li>Types containing a single item will be presented to the customer as an optional (checkbox) item.</li><li>Since all base items are required, it only makes sense to have types with single item in the optional group</li></ul></ul>";
+	if (baseItems.count === 0) {
+		baseDiv.appendChild(p);
+	} else {
+		var table  = getTableFromGroup(baseItems);
+		table.getElementsByTagName("caption")[0].innerHTML = "Inventory: Base Items";
+		baseDiv.appendChild(table);
+	}
+	var optionalDiv = document.getElementById("optionalItems");
+	if (optionalItems.count === 0 && baseItems.count > 0) {
+		p.innerHTML = p.innerHTML.replace("items", "optional items");
+		optionalDiv.appendChild(p);
+	} else {
+		var table = getTableFromgroup(optionalItems);
+		table.getElementsByTagName("caption")[0].innerHTML = "Inventory: Optional Items";
+		optionalDiv.appendChild(table);
+	}
+}
+		
 if (window.addEventListener) {
 	window.addEventListener("load", setup, false);
 }
